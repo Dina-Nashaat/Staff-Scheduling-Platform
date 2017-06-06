15c4ee8ead6ebc581c6207f444d16e3bc00cb517
Dina Nashaat <dina.nashaat@gmail.com>
2017-06-06 15:57:31 +0200

    Change Permissions table

--- database/migrations/2017_06_03_080045_create_permissions_table.php
+++ database/migrations/2017_06_03_080045_create_permissions_table.php
@@ -16,7 +16,7 @@ class CreatePermissionsTable extends Migration
         Schema::create('permissions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('permission_name');
-            $table->string('slug');
+            $table->string('description');
             $table->timestamps();   
         });
     }
--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -13,36 +13,38 @@ class PermissionsTableSeeder extends Seeder
     public function run()
     {
         $permissions = [
-                    ['permission_name'=>'schedule_admin',
-                    'slug'=> json_encode(['create'=>true, 
-                              'edit'=>true, 
-                              'delete'=>true,
-                              'view'=>true])
-                    ],
-                    ['permission_name'=>'schedule_parttime',
-                    'slug'=> json_encode(['create'=>false, 
-                              'edit'=>false, 
-                              'delete'=>false,
-                              'view'=>true])
-                    ],
-                    ['permission_name'=>'available',
-                    'slug'=> json_encode(['create'=>true, 
-                              'edit'=>true, 
-                              'delete'=>true,
-                              'view'=>true])
-                    ],
-                    ['permission_name'=>'admin_panel',
-                    'slug'=> json_encode(['create'=>true,
-                              'edit'=>true, 
-                              'delete'=>true,
-                              'view'=>true])
-                    ],
-                    ['permission_name'=>'super_panel',
-                    'slug'=> json_encode(['create'=>true, 
-                              'edit'=>true, 
-                              'delete'=>true,
-                              'view'=>true])
-                    ],
+                    ['permission_name'=>'set_availability',
+                    'description'=> "Set user's own available time and dates."],
+                    ['permission_name'=>'get_availability',
+                    'description'=> "Get user's available time and date."],
+                    ['permission_name'=>'get_availability_all',
+                    'description'=> 'Get availability of all users'],
+                    
+                    ['permission_name'=>'set_schedule_all',
+                    'description'=> 'Set schedule for all users.'],
+                    ['permission_name'=>'add_event',
+                    'description'=> "Add a new event to the schedule."],
+                    ['permission_name'=>'delete_event',
+                    'description'=> "Delete an event from the schedule."],
+                    ['permission_name'=>'edit_event',
+                    'description'=> "Edit an event in the schedule."],
+                    ['permission_name'=>'get_schedule',
+                    'description'=> "Get user's own schedule with all its details."],
+                    ['permission_name'=>'get_schedule',
+                    'description'=> "Get user's own schedule with all its details."],
+                    ['permission_name'=>'get_schedule_all',
+                    'description'=> 'Get schedule of all users'],
+
+                    ['permission_name'=>'add_user',
+                    'description'=> 'Add a new user to the system.'],
+                    ['permission_name'=>'delete_user',
+                    'description'=> 'Delete a user from the system.'],
+                    ['permission_name'=>'edit_user',
+                    'description'=> 'Edit a user in the system.'],
+                    ['permission_name'=>'view_users',
+                    'description'=> 'View all users using the system'],
+                    ['permission_name'=>'view_YLAs',
+                    'description'=> 'View only YLAs in the system'],
                  ];
         foreach($permissions as $permission){
             Permission::create($permission);
